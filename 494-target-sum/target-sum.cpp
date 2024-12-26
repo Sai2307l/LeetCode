a8be7c577ca3ebb6e4a44aadd46953baa9f3d276
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2024-12-26 08:38:56 +0530

    Time: 349 ms (33.86%) | Memory: 11.6 MB (74.02%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int count=0;
+    int arr[20];
     void back_track(vector<int>& nums,int index,int sum,int target){
         if(index==nums.size() && sum == target){
             count++;
             return;
         }
         if(index==nums.size()) return;
+        if(sum+arr[index]<target || sum-arr[index]>target) return;
         back_track(nums,index+1,sum+nums[index],target);
         back_track(nums,index+1,sum-nums[index],target);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+
+        arr[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--) arr[i]=arr[i+1]+nums[i];
+        for(auto i:arr) cout<<i<<" ";
         back_track(nums,0,0,target);
         return count;
     }
