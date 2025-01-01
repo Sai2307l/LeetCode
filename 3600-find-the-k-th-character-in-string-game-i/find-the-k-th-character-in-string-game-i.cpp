a8fdb296cdf97c19a52419d30a45b8f521d21aee
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-01-02 01:37:48 +0530

    Time: 3 ms (56.60%) | Memory: 10.8 MB (16.29%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     char transform_string(string word,int limit){
         if(word.size()>=limit) return word[limit-1];
-        string newword="";
-        for(auto i:word)newword+=i;
+        string newword=word;
         for(auto i:word)newword+=i=='z'?'a':i+1;
         return transform_string(newword,limit);
     }
