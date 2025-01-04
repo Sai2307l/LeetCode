011501b2ed35dbf3b92e81195c9d19534e55f79f
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-01-04 23:24:23 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (67.46%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,17 +3,24 @@ static const int _ =[]{
     ios::sync_with_stdio(false);
     return 0;
 }();
-class Solution {
+class Solution { 
 public:
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,INT_MAX);
-        dp[0]=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=1;j<=nums[i];j++){
-                if(i+j<nums.size())dp[i+j]=min(dp[i]+1,dp[i+j]);
-                else break;
+        if (nums.size() == 1)
+            return 0;
+        int farthest = 0;
+        int jumps = 0;
+        int end = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == end) {
+                jumps++;
+                end = farthest;
+                if (farthest >= nums.size() - 1) {
+                    break;
+                }
             }
         }
-        return dp[nums.size()-1];
+        return jumps;
     }
 };
