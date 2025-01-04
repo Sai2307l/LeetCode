60cbb87f3d6df4418b10c82926c8e3d329a89718
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-01-04 10:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (96.63%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -7,6 +7,7 @@ public:
             int l = s.find('a'+i);
             if(l==string::npos) continue;
             int r = s.find_last_of('a'+i);
+            if(r-l<=1) continue;
             for(int i=l+1;i<r;i++){
                 letters.set(s[i]-'a');
                 if(letters.count()==26) break;
