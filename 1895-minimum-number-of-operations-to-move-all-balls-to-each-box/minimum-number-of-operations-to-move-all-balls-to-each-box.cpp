074ba167aaeedf199b9f15c32ded23eb47217629
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-01-06 11:07:36 +0530

    Time: 2 ms (66.27%) | Memory: 12.5 MB (33.73%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-       int n=boxes.size(),sum=0;
-       vector<int> sol(n);
-       for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            int val = boxes[j]=='0'?0:1;
-            sol[i]+=(j-i)*(val);
+        int n=boxes.size(),sum=0;
+        vector<int> sol(n),left(n,0),right(n,0);
+        if(n==1) return {0};
+        for(int i=1;i<n;i++)left[i]=left[i]+boxes[i-1]=='1'?1+left[i-1]:left[i-1];
+        for(int i=n-2;i>=0;i--)right[i]=right[i]+boxes[i+1]=='1'?1+right[i+1]:right[i+1];
+        for(int i=1;i<n;i++){
+            left[i]=left[i-1]+left[i];
         }
-       }
-       for(int i=n-1;i>=0;i--){
-        for(int j=i-1;j>=0;j--){
-            int val = boxes[j]=='0'?0:1;
-            sol[i]+=(i-j)*(val);
+        for(int i=n-2;i>=0;i--){
+            right[i]=right[i]+right[i+1];
         }
-       }  
-       return sol;
+        for(int i=0;i<n;i++)sol[i]=left[i]+right[i];
+        return sol;
     }
 };
