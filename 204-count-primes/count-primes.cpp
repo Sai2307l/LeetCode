8df39c7080d60d7523ac459ce0c53502e633c5f5
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-02-21 10:28:52 -0800

    Time: 182 ms (68.63%) | Memory: 12.6 MB (90.80%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -4,15 +4,12 @@ public:
         vector<bool> prime(n+1,true);
         prime[0]=false;
         prime[1]=false;
-        for(int i=2;i<=n;i++){
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(prime[i]) count++;
             if(prime[i] && (long long)i*i<=n){
                 for(int j=i*i;j<=n;j+=i) prime[j]=false;
             }
-        }
-    
-        int count=0;
-        for(int i=0;i<n;i++) {
-        if(prime[i]) count++;
         }
         return count;
     }
