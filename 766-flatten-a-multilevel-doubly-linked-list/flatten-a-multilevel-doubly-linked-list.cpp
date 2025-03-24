a4f5ad3b001652f3f912e1b5dd0b15472979fd65
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-03-23 20:57:59 -0700

    Time: 6 ms (31.14%) | Memory: 11.7 MB (42.75%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -12,20 +12,29 @@ public:
 class Solution {
 public:
     Node* flatten(Node* head) {
-	for (Node* h = head; h; h = h->next)
-	{
-		if (h->child)
-		{
-			Node* next = h->next;
-			h->next = h->child;
-			h->next->prev = h;
-			h->child = NULL;
-			Node* p = h->next;
-			while (p->next) p = p->next;
-			p->next = next;
-			if (next) next->prev = p;
-		}
-	}
-	return head;
-}
+        if(head==NULL){
+            return head;
+        }
+
+        Node* curr = head;
+        while(curr != NULL){//flatten
+            if(curr->child != NULL){
+                Node * Next = curr->next;
+                curr->next = flatten(curr->child);
+                curr->next->prev = curr;
+                curr->child = NULL;
+            //find tail
+            while(curr->next != NULL){
+                curr = curr->next;
+            }// attach tail
+            if(Next != NULL){
+            curr->next = Next;
+            Next->prev = curr;
+            }
+           
+        }
+        curr = curr->next;
+        }
+    return head;
+    }
 };
