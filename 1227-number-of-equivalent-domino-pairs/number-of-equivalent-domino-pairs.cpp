93c0b89fba7606a481cc524944af72c3e8d14aef
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-05-04 23:07:20 +0530

    Time: 7 ms (41.75%) | Memory: 28.7 MB (8.75%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        map<pair<int,int>,int> freq;
-        for(auto i:dominoes){
-            int a=i[0];
-            int b=i[1];
-            auto ptr1 = freq.find(make_pair(a,b));
-            if(ptr1!=freq.end()){
-                freq[make_pair(a,b)]++;
-                continue;
-            }
-            auto ptr2 = freq.find(make_pair(b,a));
-            if(ptr2!=freq.end()){
-                freq[make_pair(b,a)]++;
-                continue;
-            }
-            freq[make_pair(a,b)]++;
-        }
+        unordered_map<int,int> freq;
         int sum=0;
-        for(auto i:freq){
-            if(i.second>1) cout<<i.second<<endl;
+        for(auto i:dominoes){
+            int a = i[0];
+            int b = i[1];
+            a>b?freq[b*10+a]++:freq[a*10+b]++; 
         }
-        for(auto i:freq){
-            if(i.second>1) sum+=(i.second*(i.second-1))/2;
-        }
+        for(auto i:freq) sum+=(i.second*(i.second-1))/2;
         return sum;
     }
 };
