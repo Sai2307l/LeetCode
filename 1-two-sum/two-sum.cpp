18af2587e1fb31f988077684e6145fc2ebc28892
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-06-20 01:04:26 +0530

    Time: 3 ms (71.22%) | Memory: 15.1 MB (15.20%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> freq;
+        for(int i=0;i<nums.size();i++){
+            if(freq.find(target-nums[i])!=freq.end()){
+                return {i,freq[target-nums[i]]};
+            }
+            else{
+                freq[nums[i]]=i;
+            }
+        }
+        return {};
+    }
+};
