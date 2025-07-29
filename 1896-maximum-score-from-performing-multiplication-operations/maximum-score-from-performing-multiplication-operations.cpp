a0267a4cf20dedc0779779e1da89937630cfc15e
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-07-29 21:42:36 +0530

    Time: 29 ms (47.46%) | Memory: 87.9 MB (62.50%) - LeetSync

--- 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -7,7 +7,6 @@ public:
     }
     int recc_sol(vector<int>& nums,vector<int>& multipliers,int left,int right,int index,vector<vector<int>>& dp){
         if(dp[left][right]!=-1) return dp[left][right];
-        cout<<left<<right<<endl;
         if(index==multipliers.size()) return dp[left][right] = 0;
         return dp[left][right] = max((nums[left]*multipliers[index])+recc_sol(nums,multipliers,left+1,right,index+1,dp),(nums[nums.size()-1-right]*multipliers[index])+recc_sol(nums,multipliers,left,right+1,index+1,dp));
     }
