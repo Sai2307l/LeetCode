2a5e27aa404ff6663cafb4741f963c2d5e44b329
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-07-29 21:42:25 +0530

    Time: 2673 ms (5.08%) | Memory: 87.9 MB (75.00%) - LeetSync

---
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int m = multipliers.size();
+        vector<vector<int>> dp(m+1,vector<int>(m+1,-1));
+        return recc_sol(nums,multipliers,0,0,0,dp);
+    }
+    int recc_sol(vector<int>& nums,vector<int>& multipliers,int left,int right,int index,vector<vector<int>>& dp){
+        if(dp[left][right]!=-1) return dp[left][right];
+        cout<<left<<right<<endl;
+        if(index==multipliers.size()) return dp[left][right] = 0;
+        return dp[left][right] = max((nums[left]*multipliers[index])+recc_sol(nums,multipliers,left+1,right,index+1,dp),(nums[nums.size()-1-right]*multipliers[index])+recc_sol(nums,multipliers,left,right+1,index+1,dp));
+    }
+};
