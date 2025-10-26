ba8569bf29ab86180e723e05648c110a8361fc98
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-10-26 10:15:12 +0530

    Time: 12 ms (58.88%) | Memory: 123.7 MB (30.79%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -0,0 +1,40 @@
+class Bank {
+public:
+    vector<long long> acc;
+    Bank(vector<long long>& balance) {
+        acc = balance; 
+    }
+    
+    bool transfer(int account1, int account2, long long money) {
+        if(account1>acc.size() || account2>acc.size()) return false;
+        if(acc[account1-1]>=money){
+            acc[account1-1]-=money;
+            acc[account2-1]+=money;
+            return true;
+        }
+        return false;
+    }
+    
+    bool deposit(int account, long long money) {
+        if(account>acc.size()) return false;
+        acc[account-1]+=money;
+        return true;
+    }
+    
+    bool withdraw(int account, long long money) {
+        if(account>acc.size()) return false;
+        if(acc[account-1]>=money){
+            acc[account-1]-=money;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
