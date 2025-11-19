32eff205253b83e1b50f92f37130d32357daea08
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-11-19 12:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (85.21%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int solve(int idx,int isUp,vector<int>&nums,vector<vector<int>>&dp){
-        if(idx == nums.size()){
-            return 0;
-        }
-        if(dp[idx][isUp]!=-1){
-            return dp[idx][isUp];
-        }
-        int notTake = solve(idx+1,isUp,nums,dp);
-        int take = 0;
-        if((isUp && nums[idx] < nums[idx - 1]) || (!isUp && nums[idx] > nums[idx - 1])){
-            take = 1 + solve(idx+1,!isUp,nums,dp);
-        }
-        return dp[idx][isUp] = max(take,notTake);
-    }
-    
     int wiggleMaxLength(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(2,-1));
-        return max(solve(1,1,nums,dp),solve(1,0,nums,dp)) + 1;
+        int up=1,down=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]){
+                up = down + 1;
+            }
+            if(nums[i]<nums[i-1]){
+                down = up + 1;
+            }
+        }
+        return max(up,down);
     }
 };
