fe763cbb2e8fa03607f1d098cdb2490f6d02b395
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2025-12-23 19:28:24 +0530

    Time: 7 ms (54.03%) | Memory: 17.3 MB (52.11%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2*k+1,0));
-        for(int j=0;j<2*k+1;j++){
-            dp[n][j]=0;
-        }
-        for(int i=0;i<n+1;i++){
-            dp[n][2*k]=0;
-        }
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<2*k;j++){
-                int profit=0;
-                if(j%2==0){
-                    int pick=-prices[i]+dp[i+1][j+1];
-                    int notPick=0+dp[i+1][j];
-                    profit=max(pick,notPick);
+        int ans  = 0, n=prices.size();
+        vector<vector<int>> dp(2*k,vector<int>(n,INT_MIN));
+        dp[0][0] = -prices[0];
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                dp[0][i] = max(dp[0][i-1],-prices[i]);
+                for(int j=1;j<2*k;j++){
+                    if(j%2==1 && j<=i)dp[j][i] = max(dp[j-1][i-1]+prices[i],dp[j][i-1]);
+                    if(j%2==0 && j<=i)dp[j][i] = dp[j-1][i-1]!=INT_MIN?max(dp[j-1][i-1]-prices[i],dp[j][i-1]):dp[j][i-1];
+                    if(j<=i)ans = max(dp[j][i],ans);
                 }
-                else{
-                    int pick=prices[i]+dp[i+1][j+1];
-                    int notPick=0+dp[i+1][j];
-                    profit=max(pick,notPick);
-                }
-                dp[i][j]=profit;
             }
         }
-        return dp[0][0];
+        
+        return ans;
     }
 };
