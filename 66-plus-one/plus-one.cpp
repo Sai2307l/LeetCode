984b1a16db46198f804631b25f971d3e663c5bde
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-01-01 10:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (50.17%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans(digits.size()+1);
+        ans[0] = 1;
+        bool con = true;
+        int idx = digits.size()-1;
+        while(con && idx>=0){
+            if(digits[idx]==9 && con){
+                digits[idx] = 0;
+            }else if(con){
+                digits[idx]++;
+                con = false;
+            }
+            idx--;
+        }
+        if(con==true){
+            for(int i=0;i<digits.size();i++){
+                ans[i+1] = digits[i];
+            }
+            return ans;
+        }else{
+            return digits;
+        }
+    }
+};
