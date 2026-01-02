bd18b059d4bf93fb6217c71a2d04bb041374d548
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-01-02 09:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (87.61%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
         map<int,int> freq;
-        int n = nums.size(),m = n/2, num;
         for(auto i:nums){
             freq[i]++;
-            if(freq[i]== m){
-                num = i;
-                break;
-            }
+            if(freq[i]==2)return i;
         }
-        return num;
+        return 0;
     }
 };
