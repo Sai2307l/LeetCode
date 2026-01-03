2d6d06193262727b14ff1717af9a6fd64631c1d7
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-01-03 10:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.00%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     const int mod  = 1e9+7;
     int numOfWays(int n) {
-        vector<long long> a(n+1),b(n+1);
-        a[1] = 6;
-        b[1] = 6;
-        for(int i=2;i<=n;i++){
-            a[i] = (2*a[i-1] + 2*b[i-1])%mod;
-            b[i] = (2*a[i-1] + 3*b[i-1])%mod;
+        long long same=6,notsame=6,currsame,currnotsame;
+        for(int i=0;i<n-1;i++){
+            currnotsame = (2*notsame + 2*same)%mod;
+            currsame = (2*notsame + 3*same)%mod;
+            same = currsame;
+            notsame  = currnotsame; 
         }
-        return (a[n]+b[n])%mod;
+        return (same+notsame)%mod;
     }
 };
