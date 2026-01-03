b3dd1c973a29a48df32af9eef289b6a785cb7883
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-01-03 10:55:18 +0530

    Time: 2 ms (48.67%) | Memory: 10.6 MB (35.67%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7;
+    const int mod  = 1e9+7;
     int numOfWays(int n) {
-        vector<long long int> a(n,6),b(n,6);
-        int tot = 0;
-        for(int i=1;i<n;i++){
-            a[i] = (2*a[i-1] + 2*b[i-1]) % mod;
-            b[i] = (2*a[i-1] + 3*b[i-1]) % mod;
+        vector<long long> a(n+1),b(n+1);
+        a[1] = 6;
+        b[1] = 6;
+        for(int i=2;i<=n;i++){
+            a[i] = (2*a[i-1] + 2*b[i-1])%mod;
+            b[i] = (2*a[i-1] + 3*b[i-1])%mod;
         }
-        return (a[n-1] + b[n-1]) % mod;
+        return (a[n]+b[n])%mod;
     }
 };
