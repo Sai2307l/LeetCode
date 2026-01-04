ac263469f53c5b70342b572d9525cfb744660452
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-01-04 09:29:51 +0530

    Time: 1 ms (96.31%) | Memory: 16.1 MB (71.43%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
-    int n = nums.size();
-    int sol = 0;
-    for(int k=0;k<n;k++){
-        int num = nums[k];
-        long long total = 1;
-        long long count = 1;
-        for (int i = 2; (long long)i * i <= num; i++) {
-            if (num % i == 0) {
-                int e = 0;
-                do {
-                    e++;
-                    num /= i;
-                } while (num % i == 0);
-                count *= e + 1;
+        int n = nums.size();
+        int sol = 0;
+        for (int k = 0; k < n; k++) {
+            int num = nums[k];
+            long long total = 1;
+            long long count = 1;
 
-                long long sum = 0, pow = 1;
-                do {
-                    sum += pow;
-                    pow *= i;
-                } while (e-- > 0);
-                total *= sum;
+            for (int i = 2; (long long)i * i <= num; i++) {
+                if (num % i == 0) {
+                    int e = 1;
+                    num/=i;
+                    while (num % i == 0){
+                        num/=i;
+                        e++;
+                    }
+                    count *= e + 1;
+
+                    long long sum = 1, pow = i;
+                    while(e-- > 0){
+                        sum += pow;
+                        pow *= i;
+                    }
+                    total *= sum;
+                }
             }
+
+            if (num > 1) {
+                total *= (1 + num);
+                count *= 2;
+            }
+
+            if (count == 4)
+                sol += total;
+                
         }
-        if (num > 1) {
-            total *= (1 + num);
-            count *= 2;
-        }
-        if(count==4) sol+=total;
-    }
-    return sol;
+        return sol;
     }
 };
