33b7acae65715fe2bee0341e94681b2871c17185
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-01-08 11:29:52 +0530

    Time: 23 ms (51.60%) | Memory: 17.9 MB (41.60%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -8,8 +8,29 @@ public:
         int notTake = max(recc_sol(dp,arr,brr,idx1,idx2-1),recc_sol(dp,arr,brr,idx1-1,idx2));
         return dp[idx1][idx2] = max(take,notTake);
     }
+    int tabulation(vector<vi>& dp,vi& arr,vi& brr){
+        int n = arr.size();
+        int m = brr.size();
+        for(int i=0;i<n;i++){
+            if(i>0)dp[i][0] = max(arr[i]*brr[0],dp[i-1][0]);
+            else dp[0][0] = arr[0]*brr[0];
+        }
+        for(int i=0;i<m;i++){
+            if(i>0)dp[0][i] = max(arr[0]*brr[i],dp[0][i-1]);
+            else dp[0][0] = arr[0]*brr[0];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j] = max(arr[i]*brr[j] + max(0,dp[i-1][j-1]),max(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+        return dp[n-1][m-1];
+    }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         vector<vi> dp(nums1.size(),vi(nums2.size(),INT_MIN));
-        return recc_sol(dp,nums1,nums2,nums1.size()-1,nums2.size()-1);
+        int ans1 = recc_sol(dp,nums1,nums2,nums1.size()-1,nums2.size()-1);
+        dp.resize(nums1.size(),vi(nums2.size(),INT_MIN));
+        int ans2 = tabulation(dp,nums1,nums2);
+        return ans2;
     }
 };
