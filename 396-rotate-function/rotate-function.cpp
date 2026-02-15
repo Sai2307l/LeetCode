b2a85291d9cc57e1e62c278805ed9ef98f0a16f8
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-02-15 21:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 99.7 MB (20.98%) - LeetSync

--- 396-rotate-function/rotate-function.cpp
+++ 396-rotate-function/rotate-function.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
-        int fx = 0;
-        for(auto i : nums){
-            fx += i;
-        }
         int n = nums.size();
-        int fi0 = 0;
-        for(int i = 0; i < n ; i++){
-            fi0 += (i*nums[i]);
+        if(n<2) return 0;
+
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int curr = 0;
+        for(int i = 0;i<n;i++){
+            curr += nums[i]*i;
         }
-        int ans = fi0 , fii = 0;
-        for(int i = 1; i < n; i++){
-            fii = fi0 + fx - n*nums[n-i];
-            ans = max(ans,fii);
-            fi0 = fii;
+        int ans = curr;
+        for(int i = 0;i<n;i++){
+            curr += sum - n*nums[n-1-i];
+            ans = max(ans,curr);
         }
         return ans;
     }
