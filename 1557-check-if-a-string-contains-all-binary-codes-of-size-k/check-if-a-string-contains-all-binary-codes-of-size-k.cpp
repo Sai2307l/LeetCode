50224cdd93ea0d7b7bf25288e751c11d3f38737f
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-02-23 18:57:17 +0530

    Time: 1204 ms (8.14%) | Memory: 193.6 MB (5.07%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    int conv(string s, int k){
-        int val = 0;
-        for(int i=k-1;i>=0;i--){
-            val+= (s[i]-'0') * pow(2,(k-1) - i);
-        }
-        return val;
-    }
     bool hasAllCodes(string s, int k) {
         if(s.size()<=k) return false;
-        set<int> nums;
+        set<string> nums;
         string substr = s.substr(0,k);
-        int num = conv(substr,k);
-        nums.insert(num);
+        nums.insert(substr);
         for(int i=k;i<s.size();i++){
             substr = substr.substr(1) + s[i];
-            num = conv(substr,k);
-            nums.insert(num);
+            nums.insert(substr);
         }
         if(nums.size()==pow(2,k))return true;
         else return false;
