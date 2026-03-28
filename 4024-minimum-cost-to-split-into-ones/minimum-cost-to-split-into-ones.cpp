a10f97b7496faa1fa9f39397234379843774bda7
Sai2307l <121341535+Sai2307l@users.noreply.github.com>
2026-03-28 20:03:44 +0530

    Time: 155 ms (5.24%) | Memory: 9.6 MB (14.05%) - LeetSync

---
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    vector<int> dp = vector<int>(501,-1);
+public:
+    int minCost(int n) {
+        if(n==1) return 0;
+        if(dp[n]!=-1)return dp[n];
+        int ans = INT_MAX;
+        for(int i=1;i<n;i++){
+            int left = minCost(i);
+            int right = minCost(n-i);
+            int cost = i*(n-i);
+            ans = min(ans,left+right+cost);
+        }
+        return dp[n] = ans;
+    }
+};
