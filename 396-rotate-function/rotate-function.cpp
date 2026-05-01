0cf5adca4e692de6f7def3e78f54ab038887e069
B. Sai Bhaskar <121341535+Sai2307l@users.noreply.github.com>
2026-05-01 17:16:27 +0530

    Time: 3 ms (53.85%) | Memory: 99.5 MB (96.82%) - LeetSync

--- 396-rotate-function/rotate-function.cpp
+++ 396-rotate-function/rotate-function.cpp
@@ -6,12 +6,10 @@ public:
         for(int i=0;i<n;i++){
             sum+=i*nums[i];
         }
-        cout<<sum<<endl;
         int sol = sum;
         for(int i=n-1;i>0;i--){
             sum+=acc-((n)*nums[i]);
             sol = max(sum,sol);
-            cout<<sum<<endl;
         }
         return sol;
     }
